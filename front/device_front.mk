2c8618ed368d996fc2232c3102edbf78b6c51152
ShevT <shev_t@mail.ru>
2013-08-18 11:10:51 +0400

    persist.sys.usb.config

--- front/device_front.mk
+++ front/device_front.mk
@@ -190,7 +190,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ADDITIONAL_DEFAULT_PROPERTIES := \
     ro.secure=0 \
     ro.allow.mock.location=1 \
-    persist.sys.usb.config=modem,nmea,mass_storage,diag \
+    persist.sys.usb.config=mtp,mass_storage \
     ro.adb.secure=0 \
     ro.debuggable=1
 
