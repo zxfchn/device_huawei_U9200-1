11694afa6ad52e033179c97dbb4d4426a4b8e8d2
ShevT <shev_t@mail.ru>
2013-09-19 17:35:47 +0400

    fix WiFi 802.11n

--- front/BoardConfig.mk
+++ front/BoardConfig.mk
@@ -109,8 +109,6 @@ WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_P2P     := "/vendor/firmware/fw_bcmdhd_p2p.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
-WIFI_DRIVER_MODULE_NAME     := "dhd"
-WIFI_BAND                   := 802_11_ABG
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
