00a995c4d0f89b8886648e947dbc0a783ee03d8e
ShevT <shev_t@mail.ru>
2014-03-11 21:03:15 +0400

    applypatch.sh: add name patch

--- front/patch/applypatch.sh
+++ front/patch/applypatch.sh
@@ -4,11 +4,13 @@
 # apply patches
 #
 
-
 dir=`cd $(dirname $0) && pwd`
 top=$dir/../../../..
 
 for patch in `ls $dir/*.patch` ; do
+	echo ""
+	echo "==> patch file: ${patch##*/}"
 	patch -p1 -N -i $patch -r - -d $top
 done
 
+cp -r ../../linaro-4.8 $top/prebuilts/gcc/linux-x86/arm/
