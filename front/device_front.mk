f631e6de870055d1ce11e60969cb6603a3311956
ShevT <shev_t@mail.ru>
2014-03-17 17:57:36 +0400

    added GhostCommander and plugins

--- front/device_front.mk
+++ front/device_front.mk
@@ -131,6 +131,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip
 
+# Ghost Commander
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/app/GhostCommander_1.51.5b2.apk:system/app/GhostCommander_1.51.5b2.apk \
+    $(LOCAL_PATH)/prebuilt/app/GhostCommander-GoogleDrive_plugin_v1.00b2.apk:system/app/GhostCommander-GoogleDrive_plugin_v1.00b2.apk \
+    $(LOCAL_PATH)/prebuilt/app/GhostCommander-Samba_plugin_v1.31.1b3.apk:system/app/GhostCommander-Samba_plugin_v1.31.1b3.apk \
+    $(LOCAL_PATH)/prebuilt/app/GhostCommander-SFTP_plugin_v1.11b3.apk:system/app/GhostCommander-SFTP_plugin_v1.11b3.apk
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
