55d9390c76bffe25bfe8697a6ec6fc08752a9d28
ShevT <shev_t@mail.ru>
2015-02-02 08:47:58 +0300

    device_front.mk: ro.sys.fw.bg_apps_limit=20

--- front/device_front.mk
+++ front/device_front.mk
@@ -173,7 +173,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=3 \
     persist.adb.notify=0 \
     ro.kernel.android.checkjni=0 \
-    sensor.loop.minms=0
+    sensor.loop.minms=0 \
+    ro.sys.fw.bg_apps_limit = 20
 
 # SGX540 is slower with the scissor optimization enabled
 PRODUCT_PROPERTY_OVERRIDES += \
