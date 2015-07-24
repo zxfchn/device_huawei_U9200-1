31ceabf628781d6fbfecd2f91d072d4ef6d7db56
ShevT <shev_t@mail.ru>
2015-07-24 10:47:12 +0300

    ramdisk: remove gpu_oc. No more in kernel

--- device.mk
+++ device.mk
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 COMMON_FOLDER := device/huawei/front
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
