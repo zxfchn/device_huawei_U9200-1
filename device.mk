c95707e857dd51d8b33888ac7652cbb09896684e
ShevT <shev_t@mail.ru>
2015-10-30 15:52:52 +0300

    front: remove BUILD_DISPLAY_ID. Is no longer needed

--- device.mk
+++ device.mk
@@ -190,11 +190,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.strictmode.visual=0 \
     persist.sys.strictmode.disable=1
 
-# Here crashes gallery
-# if ro.build.display.id is such "cm_front-userdebug 4.2.2 JDQ39E eng.shev.20130805.153138 test-keys" then gellery/camera crashshshsh
-# as well - does not crash
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=LMY48G-ShevT
-
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
