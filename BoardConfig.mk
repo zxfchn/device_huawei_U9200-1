5d2a416da8a60bd135e8f46dfb0dbbb235dda1aa
ShevT <shev_t@mail.ru>
2015-11-15 21:18:28 +0300

    BoardConfig: clean POWERHAL options

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,10 +106,6 @@ WITH_DEXPREOPT_PIC := true
 # Lights
 TARGET_PROVIDES_LIBLIGHTS := true
 
-# PowerHAL
-TARGET_PROVIDES_POWERHAL := true
-TARGET_POWERHAL_VARIANT := power.front
-
 # Use a small subset of system fonts to keep image size lower
 SMALLER_FONT_FOOTPRINT := true
 
