bdbadaa6886ec6b712069cf799a3097bd55621eb
ShevT <shev_t@mail.ru>
2015-11-20 07:15:31 +0300

    move DISABLE_CURSOR_LAYER to ti hardware repo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,10 +38,6 @@ BOARD_CANT_REALLOCATE_OMX_BUFFERS := true
 # We need BGRA_8888, instead of Android's now-default RGBA_8888.
 BOARD_EGL_WORKAROUND_BUG_10194508 := true
 
-# We don't support cursor layers, which when attempting to use them,
-# results in no cursors (mouse or otherwise) displayed on the screen.
-TARGET_DISABLE_CURSOR_LAYER := true
-
 # Use FBIOPAN_DISPLAY instead of FBIOPUT_VSCREENINFO to refresh the display.
 TARGET_USE_PAN_DISPLAY := true
 
