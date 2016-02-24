0841457f066b6dc0546543966d2dcb793dd6c05b
ShevT <shev_t@mail.ru>
2016-02-24 11:58:03 +0300

    device: remove apps limit settings

--- device.mk
+++ device.mk
@@ -180,8 +180,6 @@ ADDITIONAL_BUILD_PROPERTIES += \
     dalvik.vm.jit.codecachesize=0 \
     persist.sys.force_highendgfx=true \
     config.disable_atlas=true \
-    ro.config.max_starting_bg=8 \
-    ro.sys.fw.bg_apps_limit=16 \
     dalvik.vm.dex2oat-flags=--no-watch-dog
 
 ADDITIONAL_BUILD_PROPERTIES += \
